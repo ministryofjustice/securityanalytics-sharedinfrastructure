614a4af99dba48b177e6ea27c1964f046c0ff155
Matthew Amos <mamos@scottlogic.co.uk>
2019-04-02 13:00:25 +0100

    Deliberate mistake to check circle ci integration is fixed

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -8,6 +8,7 @@ terraform {
     dynamodb_table = "sec-an-terraform-locks"
     key            = "sec-an/terraform.tfstate"
     region         = "eu-west-2"                # london
+    profile        = "sec-an"
   }
 }
 
@@ -37,7 +38,7 @@ variable "az_limit" {
 
 provider "aws" {
   region  = "${var.aws_region}"
-  profile = "${var.app_name}"
+  profile = "sec-an"
 }
 
 #############################################
@@ -50,8 +51,4 @@ module "vpc" {
   create_private = "${var.use_private_subnets}"
   create_nat     = "${var.create_nat_gateway}"
   az_limit       = "${var.az_limit}"
-}
-
-data "aws_iam_user" "circle-ci-user" {
-  user_name = ""
 }
