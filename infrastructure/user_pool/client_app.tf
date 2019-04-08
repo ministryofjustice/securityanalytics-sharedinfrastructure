9a074fd0376f4f584e3e6c345e0dd673b955ce3b
Matthew Amos <mamos@scottlogic.com>
2019-04-08 09:56:07 +0100

    the login works, but there is an impedance mismatch between cognito which give you the token in a query param and api gateway authoriser that wants it in a header

--- infrastructure/user_pool/client_app.tf
+++ infrastructure/user_pool/client_app.tf
@@ -1,11 +1,12 @@
 resource "aws_cognito_user_pool_client" "client" {
   name       = "${terraform.workspace}-${var.app_name}-users"
-  allowed_oauth_flows = [ "code" ]
+  # TODO replace hosted ui and password auth with SRP based auth or other (i.e this becomes code)
+  allowed_oauth_flows = [ "implicit" ]
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_scopes = [ "openid", "profile", "email" ]
   callback_urls = [ "${var.api_url}" ]
   default_redirect_uri = "${var.api_url}"
-  # TODO replace hosted ui and password auth with SRP based auth or other
+  # TODO replace hosted ui and password auth with SRP based auth or other ( i.e. this becomes CUSTOM_AUTH_FLOW_ONLY)
   explicit_auth_flows = [ "USER_PASSWORD_AUTH" ]
   generate_secret = false
   read_attributes = [ "email", "family_name", "given_name" ]
