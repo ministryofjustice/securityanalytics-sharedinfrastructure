143b921176e8cb4434a741db7e1502b34a9f680b
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-04-23 12:00:26 +0100

    Feature/sa 8 add es terraform (#38)
    * Move this permission to remove the hacks we had to add to analytics

--- infrastructure/user_pool/user.tf
+++ infrastructure/user_pool/user.tf
@@ -21,6 +21,16 @@ data "aws_iam_policy_document" "sec_an_user" {
 
   statement {
     effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["es.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+  statement {
+    effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
@@ -39,4 +49,10 @@ resource "aws_iam_role" "sec_an_user" {
     app_name  = "${var.app_name}"
     workspace = "${terraform.workspace}"
   }
+}
+
+# Annoying that we have to do this hear, rather than attach it when we setup analytics
+resource "aws_iam_role_policy_attachment" "es_user" {
+  role       = "${aws_iam_role.sec_an_user.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonESCognitoAccess"
 }
