c2cbbacc64b22f82e611e74e39e7227550e46c01
Matthew Amos <mamos@scottlogic.com>
2019-05-10 11:56:05 +0100

    Add sns logging policy

---
+++ infrastructure/monitoring/sns_logging_role.tf
@@ -0,0 +1,34 @@
+data "aws_iam_policy_document" "sns_logging_trust" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type = "Service"
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "sns_logging" {
+  name = "${terraform.workspace}-${var.app_name}-sns-logging"
+  assume_role_policy = "${data.aws_iam_policy_document.sns_logging_trust.json}"
+}
+
+data "aws_iam_policy_document" "sns_logging" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:PutMetricFilter",
+        "logs:PutRetentionPolicy"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "sns_logging" {
+  policy = "${data.aws_iam_policy_document.sns_logging.json}"
+  role = "${aws_iam_role.sns_logging.id}"
+}
