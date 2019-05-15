82fd7b6fdca76b4ed5209126a0281d280bb172fa
Phil Rogers <progers@scottlogic.com>
2019-05-15 13:44:10 +0100

    fixes

--- setup-all.sh
+++ setup-all.sh
@@ -7,19 +7,19 @@ then
     exit
 fi
 cd ../securityanalytics-sharedinfrastructure
-./build.sh $1 $2
+./setup.sh $1 $2
 wait
 cd ../securityanalytics-sharedcode
-./build.sh $1 $2
+./setup.sh $1 $2
 wait
 cd ../securityanalytics-taskexecution
-./build.sh $1 $2
+./setup.sh $1 $2
 wait
 cd ../securityanalytics-analyticsplatform
-./build.sh $1 $2
+./setup.sh $1 $2
 wait
 cd ../securityanalytics-nmapscanner
-./build.sh $1 $2
+./setup.sh $1 $2
 wait
 # pause in case the user is watching output
 sleep 5
