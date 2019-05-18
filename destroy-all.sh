f3397708e607d8fa28aaf03b9d3a1acf88c121e5
Phil Rogers <progers@scottlogic.com>
2019-05-18 20:37:55 +0100

    small fixes

--- destroy-all.sh
+++ destroy-all.sh
@@ -6,20 +6,21 @@ then
     sleep 30
     exit
 fi
-cd ../securityanalytics-sharedinfrastructure
-./destroy.sh $1 $2
-wait
-cd ../securityanalytics-sharedcode
-./destroy.sh $1 $2
-wait
-cd ../securityanalytics-taskexecution
+cd ../securityanalytics-nmapscanner
 ./destroy.sh $1 $2
 wait
 cd ../securityanalytics-analyticsplatform
 ./destroy.sh $1 $2
 wait
-cd ../securityanalytics-nmapscanner
+cd ../securityanalytics-taskexecution
 ./destroy.sh $1 $2
 wait
+cd ../securityanalytics-sharedcode
+./destroy.sh $1 $2
+wait
+cd ../securityanalytics-sharedinfrastructure
+./destroy.sh $1 $2
+wait
+
 # pause in case the user is watching output
 sleep 5
