3f0ace4f447b4e0b9f9a697446832a1c23c0822e
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-06-04 11:03:49 +0100

    tf fmt

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -43,7 +43,7 @@ provider "aws" {
   # N.B. To support all authentication use cases, we expect the local environment variables to provide auth details.
   region              = var.aws_region
   allowed_account_ids = [var.account_id]
-  version = "~> 2.9"
+  version             = "~> 2.9"
 }
 
 #############################################
